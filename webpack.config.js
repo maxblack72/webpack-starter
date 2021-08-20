8872d1e95e57836535e727d456b6fcf3208f26d7
Max Rosorani <m.rosorani@gmail.com>
2021-08-20 08:38:50 +0100

    Revert "Starting version 2.0. Installed Scss css and postcss"
    This reverts commit 6ad17d28e8807b3a3a599be60f260c4e120c5b62.

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,76 @@
-const mode = process.env.NODE_ENV || "development";
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  // mode defaults to 'production' if not set
-  mode: mode,
-
   module: {
     rules: [
       {
-        test: /\.(s[ac]|c)ss$/i,
+        test: /\.html.twig$/,
         use: [
+          'raw-loader',
           {
-            loader: MiniCssExtractPlugin.loader,
-            // This is required for asset imports in CSS, such as url()
-            options: { publicPath: "" },
-          },
-          "css-loader",
-          "postcss-loader",
-          // according to the docs, sass-loader should be at the bottom, which
-          // loads it first to avoid prefixes in your sourcemaps and other issues.
-          "sass-loader",
-        ],
+            loader: 'twig-html-loader',
+            options: {
+              namespaces: {
+                'layouts': './src/views/layouts',
+                'components': './src/views/components',
+              }
+            }
+          }
+        ]
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          // without additional settings, this will reference .babelrc
-          loader: "babel-loader",
-        },
-      },
-    ],
+      test: [/.css$|.scss$/],
+      use:[
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ] 
+    },{
+      test: /\.(png|jpg|gif|svg)$/,
+      use:[{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/images'
+          }
+        }] 
+    }],
   },
-
-  plugins: [
-    new MiniCssExtractPlugin()
-  ],
-
   entry: './src/public/js/index.js',
-
-  devtool: "source-map",
-
-  // required if using webpack-dev-server
-  devServer: {
-    contentBase: "./dist",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Webpack 4 Starter',
+        filename: 'index.html',
+        template: './src/views/index.html.twig',
+        inject: true,
+        minify: {
+            removeComments: true,
+            collapseWhitespace: false
+        }
+    }),
+    new HtmlWebpackPlugin({
+        title: 'About Page',
+        filename: 'about.html',
+        template: './src/views/about.html.twig',
+        inject: true,
+        minify: {
+            removeComments: true,
+            collapseWhitespace: false
+        }
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new CopyWebpackPlugin([{
+      from:'./src/public/assets/images',
+      to:'assets/images'
+    }])
+  ]
 };
